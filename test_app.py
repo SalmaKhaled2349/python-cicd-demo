0864d1b785690f335d04d9bada1ef9fe4edeff1e
SalmaKhaled2349 <salmakhaled2349@gmail.com>
2026-05-15 21:27:31 +0300

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,10 @@
+# test_app.py — A simple test
+
+import pytest
+from app import app
+
+def test_hello():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello" in response.data
